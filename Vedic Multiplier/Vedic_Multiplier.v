d98392812fae2a0a90add94d7d49ab74487cf8a5
K Manas <94187238+manas5566@users.noreply.github.com>
2024-09-04 20:11:01 +0530

    Add files via upload

---
+++ Vedic Multiplier/Vedic_Multiplier.v
@@ -0,0 +1,116 @@
+module vedic_16x16(a, b, result);
+    input  [15:0] a,b;
+    output [31:0] result;
+    wire [31:0] result;
+    
+    wire [15:0] q0, q1, q2, q3,q4;
+    wire [23:0] q5,q6;
+    wire [15:0] temp1, temp2;
+    wire [23:0] temp3,temp4; 
+    
+vedic_8x8 V9(a[7:0]  , b[7:0] , q0[15:0]);
+vedic_8x8 V10(a[15:8], b[7:0] , q1[15:0]);
+vedic_8x8 V11(a[7:0] , b[15:8], q2[15:0]);
+vedic_8x8 V12(a[15:8], b[15:8], q3[15:0]);
+
+assign temp1= {8'b00000000, q0[15:8]};
+assign temp2= q1[15:0];
+assign temp3= {8'b00000000, q2[15:0]}; 
+assign temp4= {q3[15:0], 8'b00000000}; 
+
+sixteenbitmux_xor_fa16 A3(temp1, temp2, q4);
+sixteenbitmux_xor_fa24 A4(temp3, temp4, q5);
+sixteenbitmux_xor_fa24 A5({8'b00000000,q4}, q5, q6);
+
+assign result[7:0]= q0[7:0];
+assign result[31:8]= q6[23:0];
+
+endmodule
+
+module vedic_8x8(a, b, result);
+    input  [7:0] a,b;
+    output [15:0] result;
+    wire [15:0] result;
+    wire [7:0] q0, q1, q2, q3,q4;
+    wire [11:0] q5,q6;
+    wire [7:0] temp1, temp2;
+    wire [11:0] temp3,temp4; 
+
+vedic_4x4 V5(a[3:0], b[3:0], q0[7:0]);
+vedic_4x4 V6(a[7:4], b[3:0], q1[7:0]);
+vedic_4x4 V7(a[3:0], b[7:4], q2[7:0]);
+vedic_4x4 V8(a[7:4], b[7:4], q3[7:0]);
+
+assign temp1= {4'b0000, q0[7:4]};
+assign temp2= q1[7:0];
+assign temp3= {4'b0000, q2[7:0]}; 
+assign temp4= {q3[7:0], 4'b0000}; 
+
+sixteenbitmux_xor_fa8 A3(temp1, temp2, q4);
+sixteenbitmux_xor_fa12 A4(temp3, temp4, q5);
+sixteenbitmux_xor_fa12 A5({4'b0000,q4}, q5, q6);
+
+assign result[3:0]= q0[3:0];
+assign result[15:4]= q6[11:0];
+
+endmodule
+
+module vedic_4x4(a, b, result);
+    input  [3:0] a,b;
+    output [7:0] result;
+    wire [7:0] result;
+
+wire w1, w2, w3, w4, w5;
+wire [3:0] temp1;
+wire [5:0] temp2;
+wire [5:0] temp3;
+wire [5:0] temp4;
+wire [3:0] q0;
+wire [3:0] q1;
+wire [3:0] q2;
+wire [3:0] q3;
+wire [3:0] q4;
+wire [5:0] q5;
+wire [5:0] q6;
+
+vedic_2x2 V1(a[1:0], b[1:0], q0[3:0]);
+vedic_2x2 V2(a[3:2], b[1:0], q1[3:0]);
+vedic_2x2 V3(a[1:0], b[3:2], q2[3:0]);
+vedic_2x2 V4(a[3:2], b[3:2], q3[3:0]);
+
+assign temp1= {2'b00, q0[3:2]};
+sixteenbitmux_xor_fa4 A0(q1[3:0], temp1, q4);
+assign temp2= {2'b00, q2[3:0]};
+assign temp3= {q3[3:0], 2'b00};
+sixteenbitmux_xor_fa6 A1(temp2, temp3, q5);
+assign temp4= {2'b00, q4[3:0]};
+sixteenbitmux_xor_fa6 A2(temp4, q5, q6);
+assign result[1:0] = q0[1:0];
+assign result[7:2] = q6[5:0];
+    
+endmodule
+
+module vedic_2x2 (a, b, result);
+    input [1:0] a,b;
+    output [3:0] result;
+
+    wire [3:0] w;
+    
+    assign result[0]= a[0]&b[0];
+    assign w[0]     = a[1]&b[0];
+    assign w[1]     = a[0]&b[1];
+    assign w[2]     = a[1]&b[1];
+
+    halfAdder H0(w[0], w[1], result[1], w[3]);
+    halfAdder H1(w[2], w[3], result[2], result[3]);    
+    
+endmodule
+
+module halfAdder(a,b,sum,carry);
+    input a,b;
+    output sum, carry;
+
+assign sum   = a ^ b;
+assign carry = a & b;
+
+endmodule
